e13fa2c9864a13ff51f1f8d5b056551d02f338bd
ckraisnikovic <ckraisnikovic@gmail.com>
2016-06-10 10:39:37 +0200

    Dodavanje novog amandmana na izabrani akt - otvaranje dijaloga (koji nece da se prikaze :(( )

--- XMLproj/WebContent/controllers/amandmentsController.js
+++ XMLproj/WebContent/controllers/amandmentsController.js
@@ -1,6 +1,6 @@
 angular.module('xmlApp')
-		.controller('amandmentsController', ['$scope',
-			function($scope){
+		.controller('amandmentsController', ['$scope', '$uibModal',
+			function($scope, $uibModal){
 					$scope.acts = [ 
 							{ name: "Akt1", date: "", type: "", 
 								amandments: [{ name: "Am1"}] }, 
@@ -17,10 +17,36 @@ angular.module('xmlApp')
 						$scope.amandmentsForAct = $scope.selectedAct.amandments;
 					}
 					
-					$scope.addAmandment = function(index){
+					$scope.newAmandment = function(){
+						var modalInstance = $uibModal.open({
+								animation: false,
+								templateUrl: 'modalAddAmandment.html',
+								controller: 'modalAddAmandmentController',
+								resolve: {
+									items: function(){
+											return $scope.selectedAct;
+										}
+									}
+						});
+					}	
+				}
+		])
+		//--------------------MODAL DIALOG for adding amandment to chosen act ----------------------
+		.controller('modalAddAmandmentController', ['$scope', 'items', '$uibModalInstance',
+				function($scope, items, $uibModalInstance){
+						$scope.forAct = items;
 						
-					}
-					
-					
+						$scope.amForAct = { name: "", content: ""};
+						
+						$scope.ok = function(){
+							$scope.amForAct.name = $scope.amandmentName;
+							$scope.amForAct.content = $scope.amandmentContent;
+							
+							$scope.forAct.amandments.push($scope.amForAct);
+							$uibModalInstance.close();
+						}
+						$scope.cancel = function(){
+							$uibModalInstance.close();
+						}
 				}
 		]);
