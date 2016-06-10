c2b917be5e223b18ce27369b496e2d93e0b795ef
ckraisnikovic <ckraisnikovic@gmail.com>
2016-06-10 20:02:41 +0200

    Unos preko modala - obrisan, a dodata forma za unos amandmana

--- XMLproj/WebContent/controllers/amandmentsController.js
+++ XMLproj/WebContent/controllers/amandmentsController.js
@@ -1,6 +1,11 @@
 angular.module('xmlApp')
-		.controller('amandmentsController', ['$scope', '$uibModal',
-			function($scope, $uibModal){
+		.controller('amandmentsController', ['$scope',
+			function($scope){
+					$scope.insertTextVisible = false;
+					$scope.showTextPart = function() {
+						$scope.insertTextVisible = !$scope.insertTextVisible;
+					};
+					
 					$scope.acts = [ 
 							{ name: "Akt1", date: "", type: "", 
 								amandments: [{ name: "Am1"}] }, 
@@ -17,36 +22,16 @@ angular.module('xmlApp')
 						$scope.amandmentsForAct = $scope.selectedAct.amandments;
 					}
 					
-					$scope.newAmandment = function(){
-						var modalInstance = $uibModal.open({
-								animation: false,
-								templateUrl: 'modalAddAmandment.html',
-								controller: 'modalAddAmandmentController',
-								resolve: {
-									items: function(){
-											return $scope.selectedAct;
-										}
-									}
-						});
-					}	
-				}
-		])
-		//--------------------MODAL DIALOG for adding amandment to chosen act ----------------------
-		.controller('modalAddAmandmentController', ['$scope', 'items', '$uibModalInstance',
-				function($scope, items, $uibModalInstance){
-						$scope.forAct = items;
+					$scope.saveAmandment = function() {
 						
-						$scope.amForAct = { name: "", content: ""};
+					};
+					
+					$scope.openAmandment = function() {
 						
-						$scope.ok = function(){
-							$scope.amForAct.name = $scope.amandmentName;
-							$scope.amForAct.content = $scope.amandmentContent;
-							
-							$scope.forAct.amandments.push($scope.amForAct);
-							$uibModalInstance.close();
-						}
-						$scope.cancel = function(){
-							$uibModalInstance.close();
-						}
+					};
+					
+					$scope.deleteAmandment = function() {
+						
+					};
 				}
 		]);
