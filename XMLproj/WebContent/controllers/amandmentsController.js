49734e9c65cc37c0bc4f724b16a04ff9c842d7e9
ckraisnikovic <ckraisnikovic@gmail.com>
2016-06-10 09:35:36 +0200

    Prikaz amandmana kada se izabre neki akt

--- XMLproj/WebContent/controllers/amandmentsController.js
+++ XMLproj/WebContent/controllers/amandmentsController.js
@@ -1,22 +1,26 @@
 angular.module('xmlApp')
 		.controller('amandmentsController', ['$scope',
 			function($scope){
-					$scope.amandmentsForAct = [];
 					$scope.acts = [ 
 							{ name: "Akt1", date: "", type: "", 
-								amandments: ["Am1", "Am2", "Am3"] }, 
+								amandments: [{ name: "Am1"}] }, 
 							{ name: "Akt2", date: "", type: "",
-								amandments: ["Am1", "Am2", "Am3"] 	}, 
+								amandments: [{ name: "Am1"}, { name: "Am2"}] 	}, 
 							{ name: "Akt3", date: "", type: "",
-								amandments: ["Am1", "Am2", "Am3"] }, 
+								amandments: [{ name: "Am1"}, { name: "Am2"}, { name: "Am3"}] }, 
 							];
+							
+					$scope.selectedAct = $scope.acts[0];
+					$scope.amandmentsForAct = $scope.selectedAct.amandments;
+					
+					$scope.showAmandments = function(){
+						$scope.amandmentsForAct = $scope.selectedAct.amandments;
+					}
+					
 					$scope.addAmandment = function(index){
 						
 					}
 					
-					$scope.showAmandments = function(index){
-						console.log(index);
-						$scope.amandmentsForAct = acts[0].amandments;
-					}
+					
 				}
 		]);
