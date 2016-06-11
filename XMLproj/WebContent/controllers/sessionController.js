7f486e6d9371fa84ff8e27f6eaa04f05278b3b36
ckraisnikovic <ckraisnikovic@gmail.com>
2016-06-11 15:12:23 +0200

    Expand/collapse forme za glasanje

---
+++ XMLproj/WebContent/controllers/sessionController.js
@@ -0,0 +1,39 @@
+angular.module('xmlApp')
+		.controller('sessionController', ['$scope', '$http',
+			function($scope, $http){
+			
+			//-------------------------test data----------------------
+			var act = {
+						name: "ODLUKA O IMENOVANJU GENERALNOG SEKRETARA NARODNE SKUPŠTINE",
+						date: "6. jun 2016.",
+						type: "Odluka", 
+						amandments: [ {name: "Am1"}, {name: "Am2"}]
+						};
+			var act2 = {
+						name: "ODLUKA O BROJU I IZBORU POTPREDSEDNIKA NARODNE SKUPŠTINE",
+						date: "6. jun 2016.",
+						type: "Odluka",
+						amandments: [ {name: "Am1"}, {name: "Am2"}]
+						};
+			
+			$scope.acts = {
+							"active" : [act, act2],
+						  	"nonActive" : [act, act2]
+			    		  };		
+			
+			// Expand/collapse content for act
+			$scope.contentForActVisible = [false, false];
+			$scope.expandAct = function(index) {
+				console.log("expand " + index);
+				$scope.contentForActVisible[index] = !$scope.contentForActVisible[index];
+			};
+			$scope.testIfVisible = function(index) {
+				return $scope.contentForActVisible[index];
+			};
+			
+			$scope.sessionSubmit = function() {
+				
+			};
+		
+		}
+		]);
