78906e35578833800a54f5dc4e290e2381c7e71f
Nina <nina.marjanovic@gmail.com>
2016-06-13 18:15:32 +0200

    session get acts

--- XMLproj/WebContent/controllers/sessionController.js
+++ XMLproj/WebContent/controllers/sessionController.js
@@ -7,23 +7,13 @@ angular.module('xmlApp')
 			
 			
 			//-------------------------test data----------------------
-			var act = {
-						name: "ODLUKA O IMENOVANJU GENERALNOG SEKRETARA NARODNE SKUPŠTINE",
-						date: "6. jun 2016.",
-						type: "Odluka", 
-						amandments: [ {name: "Am1"}, {name: "Am2"}]
-						};
-			var act2 = {
-						name: "ODLUKA O BROJU I IZBORU POTPREDSEDNIKA NARODNE SKUPŠTINE",
-						date: "6. jun 2016.",
-						type: "Odluka",
-						amandments: [ {name: "Am1"}, {name: "Am2"}]
-						};
-			
-			$scope.acts = {
-							"active" : [act, act2],
-						  	"nonActive" : [act, act2]
-			    		  };		
+				//retrieving non active acts
+				$http({
+					method: "GET", 
+					url : "http://localhost:8080/XMLproj/rest/act/nonActive",
+				}).then(function(value) {
+					$scope.acts = value.data.results.bindings;
+				});		
 			
 			// Expand/collapse content for act
 			$scope.contentForActVisible = [false, false];
