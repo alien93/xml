e14813c28bb12442292845f2da503ec81d669a75
Nina <nina.marjanovic@gmail.com>
2016-06-14 19:34:53 +0200

    advanced search - acts

--- XMLproj/WebContent/controllers/searchNonActiveActsController.js
+++ XMLproj/WebContent/controllers/searchNonActiveActsController.js
@@ -1,6 +1,6 @@
 angular.module('xmlApp')
-		.controller('searchNonActiveActsController', ['$scope', '$http',
-			function($scope, $http){
+		.controller('searchNonActiveActsController', ['$scope', '$http', '$uibModal',
+			function($scope, $http, $uibModal){
 				$scope.searchText = "";
 				$scope.selectedCategory = "Sadrzaj";
 				
@@ -20,8 +20,40 @@ angular.module('xmlApp')
 				
 				//advanced search
 				$scope.advancedSearch = function(){
-					console.log("advanced search for non active acts");
-					
+					var modalInstance = $uibModal.open({
+						animation: false,
+						templateUrl: 'views/advancedSearchNonActive_m.html',
+						controller: 'advancedSearchNonActiveController',
+						resolve:{}
+				});
 				}
 			}
-		]);
+		])
+		
+		.controller('advancedSearchNonActiveController', ['$scope', '$uibModalInstance', '$filter', 
+		        function($scope, $uibModalInstance, $filter){
+			
+						$scope.search = function(){
+							var oznaka = $scope.oznaka;
+							var naziv = $scope.naziv;
+							var vrsta = $scope.vrsta;
+							var datumOd = $filter('date')($scope.datumOd, "yyyy-MM-dd");
+							var datumDo = $filter('date')($scope.datumDo, "yyyy-MM-dd");
+							var mesto = $scope.mesto;
+							var brPozitivnih = $scope.brPozitivnih;
+							var brUkupnih = $scope.brUkupnih;
+							
+							console.log(oznaka);
+							console.log(naziv);
+							console.log(vrsta);
+							console.log(datumOd);
+							console.log(datumDo);
+							console.log(mesto);
+							console.log(brPozitivnih);
+							console.log(brUkupnih);
+						}
+			
+						$scope.close = function(){
+							$uibModalInstance.close();
+						}
+		}]);
