f61385b34b2f158a943ed29b14834b9dd2c5a062
Nina <nina.marjanovic@gmail.com>
2016-06-16 14:44:05 +0200

    session - collecting necessary data

--- XMLproj/WebContent/controllers/sessionController.js
+++ XMLproj/WebContent/controllers/sessionController.js
@@ -1,206 +1,151 @@
 angular.module('xmlApp')
-		.controller('sessionController', ['$rootScope', '$scope', '$location', '$http',
-			function($rootScope, $scope, $location, $http){
-				if ($rootScope.user.role == "CITIZEN") {
-					$location.path('/prijava');
-				};	
-			
-			//retrieving non active acts
-				$http({
-					method: "GET", 
-					url : "http://localhost:8080/XMLproj/rest/act/nonActive",
-				}).then(function(value) {
-					$scope.acts = value.data.results.bindings;
-				});		
-			
-			$scope.contentForActVisible=[];
-			
-			var closeAll = function(){
-				for(i=0; i<$scope.contentForActVisible.length; i++){
-					$scope.contentForActVisible[i] = false;
-				}
+.controller('sessionController', ['$rootScope', '$scope', '$location', '$http', '$uibModal',
+                                  function($rootScope, $scope, $location, $http, $uibModal){
+	if ($rootScope.user.role == "CITIZEN") {
+		$location.path('/prijava');
+	};	
+
+	//retrieving non active acts
+	$http({
+		method: "GET", 
+		url : "http://localhost:8080/XMLproj/rest/act/nonActive",
+	}).then(function(value) {
+		$scope.acts = value.data.results.bindings;
+	});		
+
+	$scope.contentForActVisible=[];
+
+	var closeAll = function(){
+		for(i=0; i<$scope.contentForActVisible.length; i++){
+			$scope.contentForActVisible[i] = false;
+		}
+	}
+
+	$scope.expandAct = function(index, value) {
+
+		closeAll();
+		$scope.contentForActVisible[index] = true;
+		console.log(value);
+		var restUrl = "http://localhost:8080/XMLproj/rest/amendment/amendmentsForAct/" + value;
+		console.log(restUrl);
+		if($scope.contentForActVisible[index]){
+			$http({
+				method: "GET", 
+				url : restUrl,
+			}).then(function(retVal) {
+				console.log(retVal);
+				$rootScope.amendments = {"data":retVal.data.results.bindings, "glasMin":[], "glasMax":[], "primeni":[]};
+				console.log(retVal.data.results.bindings);
+			});	
+		}
+	};
+	$scope.testIfVisible = function(index) {
+		return $scope.contentForActVisible[index];
+	};
+
+	$scope.openAmendment = function(amName) {
+		$http({
+			method : "GET",
+			url : "http://localhost:8080/XMLproj/rest/amendment/amendmentId/" + amName,
+			responseType: 'arraybuffer'
+		}).then(function(result){
+			var file = new Blob([result.data], {type: 'application/pdf'});
+			var fileURL = URL.createObjectURL(file);
+			window.open(fileURL);
+		}, function(reason){
+			console.log(JSON.stringify(reason));
+		});
+	};
+
+	$scope.openAct = function(docName){
+		console.log(docName);
+		$http({
+			method : "GET",
+			url : "http://localhost:8080/XMLproj/rest/act/nonActiveId/" + docName,
+			responseType: 'arraybuffer'
+		}).then(function(result){
+			var file = new Blob([result.data], {type: 'application/pdf'});
+			var fileURL = URL.createObjectURL(file);
+			window.open(fileURL);
+		}, function(reason){
+			console.log(JSON.stringify(reason));
+		});
+	};
+
+	$scope.acceptAmendment = function(amendmentId){
+		console.log(amendmentId);
+	}
+
+
+	//---------------------------------------------------session--------------------------------------
+
+
+
+	//proveri da li postoje amandmani koje treba prihvatiti
+	var acceptingAmendment = function(){
+		var retVal = false;
+		for(var i=0; i<$scope.amendments.data.length; i++){
+			if($scope.amendments.primeni[i] == true){
+				retVal = true;
+				break;
 			}
-			
-			$scope.expandAct = function(index, value) {
-				
-				closeAll();
-				$scope.contentForActVisible[index] = true;
-				console.log(value);
-				var restUrl = "http://localhost:8080/XMLproj/rest/amendment/amendmentsForAct/" + value;
-				console.log(restUrl);
-				if($scope.contentForActVisible[index]){
-					$http({
-						method: "GET", 
-						url : restUrl,
-					}).then(function(retVal) {
-						console.log(retVal);
-						$scope.amendments = {"data":retVal.data.results.bindings, "glasMin":[], "glasMax":[], "primeni":[]};
-						console.log(retVal.data.results.bindings);
-					});	
-				}
-			};
-			$scope.testIfVisible = function(index) {
-				return $scope.contentForActVisible[index];
-			};
-			
-			$scope.openAmendment = function(amName) {
-				$http({
-					method : "GET",
-					url : "http://localhost:8080/XMLproj/rest/amendment/amendmentId/" + amName,
-					responseType: 'arraybuffer'
-				}).then(function(result){
-					var file = new Blob([result.data], {type: 'application/pdf'});
-				    var fileURL = URL.createObjectURL(file);
-				    window.open(fileURL);
-				}, function(reason){
-					console.log(JSON.stringify(reason));
-				});
-			};
-			
-			$scope.openAct = function(docName){
-				console.log(docName);
-				$http({
-					method : "GET",
-					url : "http://localhost:8080/XMLproj/rest/act/nonActiveId/" + docName,
-					responseType: 'arraybuffer'
-				}).then(function(result){
-					var file = new Blob([result.data], {type: 'application/pdf'});
-				    var fileURL = URL.createObjectURL(file);
-				    window.open(fileURL);
-				}, function(reason){
-					console.log(JSON.stringify(reason));
-				});
-			};
-			
-			$scope.acceptAmendment = function(amendmentId){
-				console.log(amendmentId);
-			}
-			
-			
-			//---------------------------------------------------session--------------------------------------
-			
-			var changeActsCollection = function(result1){
-				$http({
-					method : "POST",
-					url : "http://localhost:8080/XMLproj/rest/act/changeCollection/doneti",
-					headers : {
-						"Content-Type": "application/xml"
+		}
+		return retVal;
+	}
+
+	/**
+	 * Submit session data
+	 */
+	$scope.sessionSubmit = function(actId) {
+		//ukoliko se akt prihvata u celini, automatski prihvati i sve amandmane (promeni kolekcije)
+		if($scope.uCelini == true){
+			var modalInstance = $uibModal.open({
+				animation: false,
+				templateUrl: 'views/actData_m.html',
+				controller: 'actDataController',
+				resolve:{
+					actId : function(){
+						return actId
 					},
-					data : result1.data
-				});
-			}
-			
-			var changeAmendmentsCollection = function(result, result1){
-				$http({
-					method : "POST",
-					url : "http://localhost:8080/XMLproj/rest/act/updateAct/" + actId,
-					headers : {
-						"Content-Type" : "application/xml"
-					},
-					data : result.data
-				}).then(function(result){
-					//promeni kolekciju akta
-					changeActsCollection(result1);
-				})
-			}
-			
-			var changeAmendmentsStatus = function(result, result1){
-				$http({
-					method : "POST",
-					url : "http://localhost:8080/XMLproj/rest/amendment/changeStatus/prihvacen",
-					headers : {
-						"Content-Type" : "application/xml"
-					},
-					data : result.data
-				}).then(function(result){
-					//promeni kolekciju amandmana
-					changeAmendmentsCollection(result, result1);
-				})
-			}
-			
-			var getXmlByAmendmentsId = function(amendmentId, result, result1){
-				$http({
-					method : "GET",
-					url : "http://localhost:8080/XMLproj/rest/amendment/xmlById/" + amendmentId,
-				}).then(function(result){
-					//prosledi sadrzaj metodi koja vrsi izmenu statusa amandmana
-					changeAmendmentsStatus(result, result1);
-					
-				})
-			}
-			
-			var changeActsStatus = function(result){
-				$http({
-					method : "POST",
-					url : "http://localhost:8080/XMLproj/rest/act/changeStatus/donet",
-					headers : {
-						"Content-Type": "application/xml"
-					},
-					data : result.data
-				}).then(function(result1){
-					//primeni amandmane
-					console.log(result1.data);
-					for(var i=0; i<$scope.amendments.data.length; i++){
-						var amendmentId = $scope.amendments.data[i].oznakaAmandman.value;
-						//dobavi sadrzaj xml fajla na oznovu oznaka amandmana
-						getXmlByAmendmentsId(amendmentId, result, result1);
-					}
-				}, function(reason){
-					console.log(JSON.stringify(reason));
-				});
-			}
-			
-			var acceptActAndAmendments = function(actId){
-				$http({
-					method : "GET",
-					url : "http://localhost:8080/XMLproj/rest/act/xmlById/" + actId,
-				}).then(function(result){
-					console.log(result.data);
-					//prosledi sadrzaj metodi koja vrsi izmenu statusa akta
-					changeActsStatus(result);
-				}, function(reason){
-					console.log(JSON.stringify(reason));
-				});
-			}
-			
-			//proveri da li postoje amandmani koje treba prihvatiti
-			var acceptingAmendment = function(){
-				var retVal = false;
-				for(var i=0; i<$scope.amendments.data.length; i++){
-					if($scope.amendments.primeni[i] == true){
-						retVal = true;
-						break;
+					scenario : function(){
+						return 1;
 					}
 				}
-				return retVal;
+			});
+		}
+		else{
+			//Akt se prihvata u nacelu, promeni status u 'donet' i primeni amandmane koje treba primeniti
+			if($scope.uNacelu == true){
+				var modalInstance = $uibModal.open({
+					animation: false,
+					templateUrl: 'views/actData_m.html',
+					controller: 'actDataController',
+					resolve:{
+						actId : function(){
+							return actId
+						},
+						scenario : function(){
+							return 2;
+						}
+					}
+				});
 			}
-			
-			$scope.sessionSubmit = function(actId) {
-				//ukoliko se akt prihvata u celini, automatski prihvati i sve amandmane (promeni kolekcije)
-				if($scope.uCelini == true){
-					acceptActAndAmendments(actId);					
+			else{
+				//postoje amandmani koje treba primeniti
+				if(acceptingAmendment()){	
+					acceptAmendments(actId);
 				}
+				//Smesti akt u povucene i amandmane u odbijene
 				else{
-					//Akt se prihvata u nacelu, promeni status u 'donet' i primeni amandmane koje treba primeniti
-					if($scope.uNacelu == true){
-						acceptActAndSomeAmendments(actId);
-					}
-					else{
-						//postoje amandmani koje treba primeniti
-						if(acceptingAmendment()){	
-							acceptAmendments(actId);
-						}
-						//Smesti akt u povucene i amandmane u odbijene
-						else{
-							rejectAll(actId);
-						}
-					}
+					rejectAll(actId);
 				}
-				
-				//-----------------------------------------/session--------------------------------------------
-				
-				
-			/*	console.log(actId);
+			}
+		}
+
+		//-----------------------------------------/session--------------------------------------------
+
+
+		/*	console.log(actId);
 				console.log($scope.aktGlasMin);
 				console.log($scope.aktGlasMax);
 				console.log($scope.uNacelu);
@@ -211,8 +156,111 @@ angular.module('xmlApp')
 					console.log($scope.amendments.primeni[i]);
 				}
 				console.log($scope.uCelini);*/
-			};
-		
-		
+	};
+
+
+}
+])
+
+.controller('actDataController', ['$scope', '$rootScope', '$uibModalInstance', '$http', 'actId', 'scenario',  
+                                  function($scope, $rootScope, $uibModalInstance, $http, actId, scenario){
+
+
+	var changeActsCollection = function(result1){
+		$http({
+			method : "POST",
+			url : "http://localhost:8080/XMLproj/rest/act/changeCollection/doneti",
+			headers : {
+				"Content-Type": "application/xml"
+			},
+			data : result1.data
+		});
+	}
+
+	var changeAmendmentsCollection = function(result, result1, actId){
+		$http({
+			method : "POST",
+			url : "http://localhost:8080/XMLproj/rest/act/updateAct/" + actId,
+			headers : {
+				"Content-Type" : "application/xml"
+			},
+			data : result.data
+		}).then(function(result){
+			//promeni kolekciju akta
+			changeActsCollection(result1);
+		})
+	}
+
+	var changeAmendmentsStatus = function(result, result1, actId){
+		$http({
+			method : "POST",
+			url : "http://localhost:8080/XMLproj/rest/amendment/changeStatus/prihvacen",
+			headers : {
+				"Content-Type" : "application/xml"
+			},
+			data : result.data
+		}).then(function(result){
+			//promeni kolekciju amandmana
+			changeAmendmentsCollection(result, result1, actId);
+		})
+	}
+
+	var getXmlByAmendmentsId = function(amendmentId, result, result1, actId){
+		$http({
+			method : "GET",
+			url : "http://localhost:8080/XMLproj/rest/amendment/xmlById/" + amendmentId,
+		}).then(function(result){
+			//prosledi sadrzaj metodi koja vrsi izmenu statusa amandmana
+			changeAmendmentsStatus(result, result1, actId);
+
+		})
+	}
+
+	var changeActsStatus = function(result, actId){
+		$http({
+			method : "POST",
+			url : "http://localhost:8080/XMLproj/rest/act/changeStatus/donet",
+			headers : {
+				"Content-Type": "application/xml"
+			},
+			data : result.data
+		}).then(function(result1){
+			//primeni amandmane
+			console.log(result1.data);
+			for(var i=0; i<$rootScope.amendments.data.length; i++){
+				var amendmentId = $rootScope.amendments.data[i].oznakaAmandman.value;
+				//dobavi sadrzaj xml fajla na oznovu oznaka amandmana
+				getXmlByAmendmentsId(amendmentId, result, result1, actId);
+			}
+		}, function(reason){
+			console.log(JSON.stringify(reason));
+		});
+	}
+
+	var acceptActAndAmendments = function(actId){
+		$http({
+			method : "GET",
+			url : "http://localhost:8080/XMLproj/rest/act/xmlById/" + actId,
+		}).then(function(result){
+			console.log(result.data);
+			//prosledi sadrzaj metodi koja vrsi izmenu statusa akta
+			changeActsStatus(result, actId);
+		}, function(reason){
+			console.log(JSON.stringify(reason));
+		});
+	}
+
+	$scope.close = function(){
+		if(scenario == 1){
+			acceptActAndAmendments(actId);
+			$uibModalInstance.close();
 		}
-		]);
+		else if(scenario == 2){
+			acceptActAndSomeAmendments(actId);
+			$uibModalInstance.close();
+		}
+		else{
+			$uibModalInstance.close();
+		}
+	}
+}]);
