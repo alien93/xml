03b37839ce021e951484694281166897a755bc40
Nina <nina.marjanovic@gmail.com>
2016-06-16 12:55:44 +0200

    session - removing

--- XMLproj/WebContent/controllers/sessionController.js
+++ XMLproj/WebContent/controllers/sessionController.js
@@ -76,76 +76,98 @@ angular.module('xmlApp')
 				console.log(amendmentId);
 			}
 			
+			
+			//---------------------------------------------------session--------------------------------------
+			
+			var changeActsCollection = function(result1){
+				$http({
+					method : "POST",
+					url : "http://localhost:8080/XMLproj/rest/act/changeCollection/doneti",
+					headers : {
+						"Content-Type": "application/xml"
+					},
+					data : result1.data
+				});
+			}
+			
+			var changeAmendmentsCollection = function(result, result1){
+				$http({
+					method : "POST",
+					url : "http://localhost:8080/XMLproj/rest/act/updateAct/" + actId,
+					headers : {
+						"Content-Type" : "application/xml"
+					},
+					data : result.data
+				}).then(function(result){
+					//promeni kolekciju akta
+					changeActsCollection(result1);
+				})
+			}
+			
+			var changeAmendmentsStatus = function(result, result1){
+				$http({
+					method : "POST",
+					url : "http://localhost:8080/XMLproj/rest/amendment/changeStatus/prihvacen",
+					headers : {
+						"Content-Type" : "application/xml"
+					},
+					data : result.data
+				}).then(function(result){
+					//promeni kolekciju amandmana
+					changeAmendmentsCollection(result, result1);
+				})
+			}
+			
+			var getXmlByAmendmentsId = function(amendmentId, result, result1){
+				$http({
+					method : "GET",
+					url : "http://localhost:8080/XMLproj/rest/amendment/xmlById/" + amendmentId,
+				}).then(function(result){
+					//prosledi sadrzaj metodi koja vrsi izmenu statusa amandmana
+					changeAmendmentsStatus(result, result1);
+					
+				})
+			}
+			
+			var changeActsStatus = function(result){
+				$http({
+					method : "POST",
+					url : "http://localhost:8080/XMLproj/rest/act/changeStatus/donet",
+					headers : {
+						"Content-Type": "application/xml"
+					},
+					data : result.data
+				}).then(function(result1){
+					//primeni amandmane
+					console.log(result1.data);
+					for(var i=0; i<$scope.amendments.data.length; i++){
+						var amendmentId = $scope.amendments.data[i].oznakaAmandman.value;
+						//dobavi sadrzaj xml fajla na oznovu oznaka amandmana
+						getXmlByAmendmentsId(amendmentId, result, result1);
+					}
+				}, function(reason){
+					console.log(JSON.stringify(reason));
+				});
+			}
+			
+			var acceptActAndAmendments = function(actId){
+				$http({
+					method : "GET",
+					url : "http://localhost:8080/XMLproj/rest/act/xmlById/" + actId,
+				}).then(function(result){
+					console.log(result.data);
+					//prosledi sadrzaj metodi koja vrsi izmenu statusa akta
+					changeActsStatus(result);
+				}, function(reason){
+					console.log(JSON.stringify(reason));
+				});
+			}
+			
+			
 			$scope.sessionSubmit = function(actId) {
 				//ukoliko se akt prihvata u celini, automatski prihvati i sve amandmane (promeni kolekcije)
 				if($scope.uCelini == true){
-					console.log("Akt se prihvata u celini, promeni status u 'donet' i primeni sve amandmane");
-					//dobavi sadrzaj xml fajla na osnovu oznake akta
-					$http({
-						method : "GET",
-						url : "http://localhost:8080/XMLproj/rest/act/xmlById/" + actId,
-					}).then(function(result){
-						console.log(result.data);
-						//prosledi sadrzaj metodi koja vrsi izmenu statusa akta
-						$http({
-							method : "POST",
-							url : "http://localhost:8080/XMLproj/rest/act/changeStatus/donet",
-							headers : {
-								"Content-Type": "application/xml"
-							},
-							data : result.data
-						}).then(function(result1){
-							//primeni amandmane
-							console.log(result1.data);
-							for(var i=0; i<$scope.amendments.data.length; i++){
-								var amendmentId = $scope.amendments.data[i].oznakaAmandman.value;
-								//dobavi sadrzaj xml fajla na oznovu oznaka amandmana
-								$http({
-									method : "GET",
-									url : "http://localhost:8080/XMLproj/rest/amendment/xmlById/" + amendmentId,
-								}).then(function(result){
-									console.log(result.data);
-									//prosledi sadrzaj metodi koja vrsi izmenu statusa amandmana
-									$http({
-										method : "POST",
-										url : "http://localhost:8080/XMLproj/rest/amendment/changeStatus/prihvacen",
-										headers : {
-											"Content-Type" : "application/xml"
-										},
-										data : result.data
-									}).then(function(result){
-										//promeni kolekciju amandmana
-										$http({
-											method : "POST",
-											url : "http://localhost:8080/XMLproj/rest/act/updateAct/" + actId,
-											headers : {
-												"Content-Type" : "application/xml"
-											},
-											data : result.data
-										}).then(function(result){
-											//promeni kolekciju akta
-											$http({
-												method : "POST",
-												url : "http://localhost:8080/XMLproj/rest/act/changeCollection/doneti",
-												headers : {
-													"Content-Type": "application/xml"
-												},
-												data : result1.data
-											});
-										})
-									})
-								})
-							}
-							
-						}, function(reason){
-							console.log(JSON.stringify(reason));
-						});
-						
-					}, function(reason){
-						console.log(JSON.stringify(reason));
-					});
-					
-					
+					acceptActAndAmendments(actId);					
 				}
 				else{
 					if($scope.uNacelu == true){
@@ -160,6 +182,8 @@ angular.module('xmlApp')
 						}
 					}
 				}
+				
+				//-----------------------------------------/session--------------------------------------------
 				
 				
 			/*	console.log(actId);
